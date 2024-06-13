e94e83510432748b66e963b531b08d3d3d8c16dd
Zain Ahmed <154746358+Zain4YESA@users.noreply.github.com>
2024-06-14 00:55:25 +0500

    Create Leap Year Calculator.js

---
+++ Leap Year Calculator.js
@@ -0,0 +1,15 @@
+let Years = parseInt(prompt("Enter the number of years"));
+console.log("Years:" + Years);
+
+let Count_the_years_divisible_by_4 = (Years/4);
+console.log("Count the years:" + Count_the_years_divisible_by_4);
+let Subtract_the_years_divisible_by_100 = (Years/100);
+console.log("Subtract the years:" + Subtract_the_years_divisible_by_100);
+let Add_the_years_divisible_by_400 = (Years/400);
+console.log("Add the years:" + Add_the_years_divisible_by_400);
+
+
+
+let Number_of_leap_years = Count_the_years_divisible_by_4 - Subtract_the_years_divisible_by_100 + Add_the_years_divisible_by_400;
+;
+console.log("Number of Leap Years in " + Years, "years:" + Number_of_leap_years );
